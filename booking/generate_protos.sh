948a23c1c7594023aa4b20b578fdcf16a2e2b5ea
NicolasChusseau <nchusseau@free.fr>
2024-11-15 08:31:33 +0100

    fix script

--- booking/generate_protos.sh
+++ booking/generate_protos.sh
@@ -1,11 +1,5 @@
-rm showtime_pb2.py
-rm showtime_pb2_grpc.py
 python3 -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. showtime.proto
 
-rm booking_pb2.py
-rm booking_pb2_grpc.py
 python3 -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. booking.proto
 
-rm to_import_pb2.py
-rm to_import_pb2_grpc.py
 python3 -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. to_import.proto
